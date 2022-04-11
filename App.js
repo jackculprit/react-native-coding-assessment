2f6499ac3ed6e315d99d2415c5555926bd20cc86
JBader <jbader@JBaders-MacBook-Pro.local>
2022-04-11 13:19:31 -0600

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useEffect } from 'react';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 export default function App() {
+  useEffect(() => {
+    setInterval(() => {
+      recordTemperature(getRandomInt(-100, 100));
+    }, 1000);
+  }, [])
+
+  function recordTemperature(temp) {
+
+  }
+
+  function avgTempPast24Hours() {
+
+  }
+
+  function maxTempPast24Hours() {
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
